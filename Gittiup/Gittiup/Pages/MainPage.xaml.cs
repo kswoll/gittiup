e9850d6cef7a01ea2a50aa4563eea1c74af42d88
Kirk Woll <kirk@plangrid.com>
2018-06-03 14:51:59 -0700

    Initial commit

---
+++ Gittiup/Gittiup/Pages/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace Gittiup.Pages
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+//                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavigationView_Navigate(item);
+            }
+        }
+
+        private void NavigationView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "repositories":
+                    ContentFrame.Navigate(typeof(RepositoriesPage));
+                    break;
+            }
+        }
+    }
+}
