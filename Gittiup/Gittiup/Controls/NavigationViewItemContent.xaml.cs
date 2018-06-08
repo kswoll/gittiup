070f595f240c787a7e6ac6b7254d748048a3b168
Kirk Woll <kirk@plangrid.com>
2018-06-07 18:13:16 -0700

    make resuable control for navigation item content

---
+++ Gittiup/Gittiup/Controls/NavigationViewItemContent.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using FontAwesome.UWP;
+
+namespace Gittiup.Controls
+{
+    public sealed partial class NavigationViewItemContent
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof (Icon), typeof(FontAwesomeIcon), typeof (FontAwesome.UWP.FontAwesome), new PropertyMetadata((object) FontAwesomeIcon.None));
+
+        public NavigationViewItemContent()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>Gets or sets the FontAwesome icon</summary>
+        public FontAwesomeIcon Icon
+        {
+            get => (FontAwesomeIcon)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+    }
+}
