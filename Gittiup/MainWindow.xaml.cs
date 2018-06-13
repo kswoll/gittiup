963ab9350547605aa89adb66e351e67690f33cdd
Kirk Woll <kirk@plangrid.com>
2018-06-13 14:45:24 -0700

    back to wpf

---
+++ Gittiup/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Gittiup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
