0eeb95a501481166b84c420641fb78fe3b25f7c2
Kirk Woll <kirk.woll@gmail.com>
2018-06-29 17:18:55 -0700

    wip

---
+++ Gittiup.Library.Tests/Utils/DiffLineGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using Gittiup.Library.Utils;
+using NUnit.Framework;
+
+namespace Gittiup.Library.Tests.Utils
+{
+    [TestFixture]
+    public class DiffLineGeneratorTests
+    {
+        [Test]
+        public void EmptyState()
+        {
+            var oldContent = "";
+            var newContent = "";
+            var diffs = DiffLineGenerator.GenerateLineDiffs(oldContent, newContent);
+            Assert.AreEqual(1, diffs.Count);
+            Assert.AreEqual(0, diffs[0].Count);
+        }
+
+        [Test]
+        public void InsertNewLine()
+        {
+            var oldContent = "a\r\nc";
+            var newContent = "a\r\nb\r\nc";
+            var diffs = DiffLineGenerator.GenerateLineDiffs(oldContent, newContent);
+            Assert.AreEqual(3, diffs.Count);
+            var line = diffs[1];
+            Assert.AreEqual(1, line.Count);
+            Assert.AreEqual("b", line[0].Text);
+            Assert.IsTrue(line[0].Operation.IsInsert);
+        }
+    }
+}
---
+++ Gittiup.Library/Utils/DiffLineGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using DiffMatchPatch;
+
+namespace Gittiup.Library.Utils
+{
+    public static class DiffLineGenerator
+    {
+        public static ImmutableList<DiffLine> GenerateLineDiffs(string oldText, string newText)
+        {
+            List<Diff> diffs;
+            if (oldText == null)
+            {
+                diffs = new List<Diff>
+                {
+                    new Diff(newText, Operation.Insert)
+                };
+            }
+            else
+            {
+                var differ = DiffMatchPatchModule.Default;
+                diffs = differ.DiffMain(oldText, newText);
+                differ.DiffCleanupSemantic(diffs);
+            }
+
+//            var differ = new DiffMatchPatch.DiffMatchPatch(.5f, 32, 4, 0.5f, 1000, 32, 0.5f, 4);
+
+            var lines = new List<DiffLine>();
+            var currentLine = new DiffLine();
+            Diff currentLineInitialDiff = null;
+            foreach (var diff in diffs)
+            {
+                var diffLines = diff.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                var newLine = false;
+                if (currentLine.All(x => x.Text.Length == 0))
+                {
+                    currentLineInitialDiff = diff;
+                }
+
+                foreach (var line in diffLines)
+                {
+                    if (newLine)
+                    {
+                        if (currentLineInitialDiff == null || Equals(currentLineInitialDiff, diff))
+                        {
+                            currentLine.Operation = diff.Operation;
+                        }
+                        lines.Add(currentLine);
+                        currentLine = new DiffLine();
+                        currentLineInitialDiff = diff;
+                    }
+
+                    if (line.Length > 0)
+                    {
+                        currentLine.Add(new Diff(line, diff.Operation));
+                    }
+                    newLine = true;
+                }
+            }
+
+            if (currentLine.Select(x => x.Operation).ToImmutableHashSet().Count == 1)
+                currentLine.Operation = currentLine[0].Operation;
+            lines.Add(currentLine);
+
+            return lines.ToImmutableList();
+        }
+    }
+}
